c122b1ab281cdc8e29c50d35eb90b52bf498d7fb
OleksiyenkoV <54861261+OleksiyenkoV@users.noreply.github.com>
2020-03-22 17:25:27 +0200

    Add files via upload

---
+++ telegram-bot.js
@@ -0,0 +1,26 @@
+﻿var TelegramBot = require('node-telegram-bot-api');
+
+const token = '1147541859:AAEdivgSKW97qWA_v-Vn38eXwESf1qnt7x4';
+const bot = new TelegramBot(token, {polling: true});
+
+const notes = [];
+
+bot.onText(/напиши (.+) в (.+)/, (msg, match) => {
+    const userId = msg.from.id;
+    const text = match[1];
+    const time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Пфф! Ок. Я напишу про це, но для начала 100 грн карту :)');
+});
+
+setInterval(() => {
+    for (let i = 0; i < notes.length; i++) {
+    const curDate = `${new Date().getHours()}:${new Date().getMinutes()}`;
+    if (notes[i]['time'] === curDate) {
+      bot.sendMessage(notes[i]['uid'], `Пишу вам, ви повинні: ${notes[i]['text']} щас.`);
+      notes.splice(i, 1);
+    }
+  }
+}, 1000);
